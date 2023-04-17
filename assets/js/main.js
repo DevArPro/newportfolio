874ededf04c956ee570348c562bebbd571a8bffd
DevɅr <105812278+DevArPro@users.noreply.github.com>
2023-04-17 20:11:15 +0200

    Màj JavaScript (Opti/Comments)

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,4 @@
+/*-- Variables --*/
 const navMenu = document.getElementById("nav-menu"),
   toggleMenu = document.getElementById("nav-toggle"),
   body = document.getElementById("body"),
@@ -6,19 +7,40 @@ const navMenu = document.getElementById("nav-menu"),
   buttonUp = document.getElementById("bouton-page-up"),
   contenu = document.querySelectorAll(".contenu"),
   loader = document.querySelector('.loader');
-let index = 0;
+let index = 0; /*-> Slider*/
+/*De nombreuses variables non utilisées, en attente de la refonte du code correspondant au menu mobile*/
 
+/*----- PRELOADER -----*/
 window.addEventListener('load', () => {
-  loader.classList.add('fondu-sortant');
   loader.classList.remove('loader-index');
 });
 
+/*----- STICKY HEADER & BOUTON HAUT DE PAGE -----*/
 window.addEventListener("scroll", function () {
   const header = document.querySelector("header");
   header.classList.toggle("sticky", window.scrollY > 0);
   buttonUp.classList.toggle("block", window.scrollY > 0);
 });
 
+/*----- MENU MOBILE -----*/
+function menuMobile() {
+  var x = document.getElementById("nav-menu");
+  if (x.className === "nav") {
+    x.className += " responsive";
+  } else {
+    x.className = "nav";
+  }
+}
+function linkMenuMobile() {
+  var x = document.getElementById("nav-menu");
+  if (x.className === "responsive") {
+    x.className = "none";
+  } else {
+    x.className = "nav";
+  }
+}
+
+/*----- SLIDER (index.html) -----*/
 onglets.forEach((onglet) => {
   onglet.addEventListener("click", () => {
     if (onglet.classList.contains("onglet-actif")) {
@@ -51,20 +73,3 @@ toggleMenu.addEventListener("click", () => {
   body.classList.toggle("hidden");
 });*/
 
-/*Menu Responsive Mobile*/
-function menuMobile() {
-  var x = document.getElementById("nav-menu");
-  if (x.className === "nav") {
-    x.className += " responsive";
-  } else {
-    x.className = "nav";
-  }
-}
-function linkMenuMobile() {
-  var x = document.getElementById("nav-menu");
-  if (x.className === "responsive") {
-    x.className = "none";
-  } else {
-    x.className = "nav";
-  }
-}
